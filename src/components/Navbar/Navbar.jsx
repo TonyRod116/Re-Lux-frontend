338b449ab0ef46de65c9ab02681377867203a016
Katie Hill <khill.fr.gr@gmail.com>
2025-08-09 14:05:33 +0100

    Basic homepage styling added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <nav>
       <nav id="main-pages">
-      <a href="/">Home</a>
-      <a href="/Items">Items</a>
+      <a href="/">Re-Lux</a>
+      <a href="/Items">Men</a>
+      <a href="/Items">Women</a>
+      <a href="/Items">Accessories</a>
+      <a href="/Items">Tech</a>
+      <a href="/Items">Lifestyle</a>
       </nav>
       <nav id="user-access">
       <a href="/sign-in">Sign In</a>
