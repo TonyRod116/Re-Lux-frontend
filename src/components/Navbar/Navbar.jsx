86863f2d543b0717d44a093f2235e9b099a9c362
TonyRod116 <tony.rod.bcn@gmail.com>
2025-08-09 17:28:14 +0200

    small profile page changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,18 +2,20 @@ import { useContext } from 'react'
 import { UserContext } from '../../Contexts/UserContext'
 import './Navbar.css'
 
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
   const { user, signOut } = useContext(UserContext)
 
   return (
     <nav>
       <nav id="main-pages">
-      <a href="/" class="home-link">Re-Lux</a>
-      <a href="/Items" class="page-link">Men</a>
-      <a href="/Items" class="page-link">Women</a>
-      <a href="/Items" class="page-link">Accessories</a>
-      <a href="/Items" class="page-link">Tech</a>
-      <a href="/Items" class="page-link">Lifestyle</a>
+      <Link to="/" class="home-link">Re-Lux</Link>
+      <Link to="/items" class="page-link">Men</Link>
+      <Link to="/items" class="page-link">Women</Link>
+      <Link to="/items" class="page-link">Accessories</Link>
+      <Link to="/items" class="page-link">Tech</Link>
+      <Link to="/items" class="page-link">Lifestyle</Link>
       </nav>
       <nav id="user-access">
       {user 
