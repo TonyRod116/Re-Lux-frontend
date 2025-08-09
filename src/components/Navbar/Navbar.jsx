6875e7b9bc3fe672658f1d6cf0270d01e759f6cd
Katie Hill <khill.fr.gr@gmail.com>
2025-08-09 13:15:42 +0100

    Navbar & Footer styling added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,10 +4,14 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <nav>
+      <nav id="main-pages">
       <a href="/">Home</a>
       <a href="/Items">Items</a>
+      </nav>
+      <nav id="user-access">
       <a href="/sign-in">Sign In</a>
       <a href="/sign-up">Sign Up</a>
+      </nav>
     </nav>
   )
 }
