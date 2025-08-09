72a96ecfd14e10c2d7e227a9f4025cc836390ab3
Katie Hill <khill.fr.gr@gmail.com>
2025-08-09 17:43:51 +0100

    Axios calls for items added to services folder

---
+++ src/services/items.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const BASE_URL = import.meta.env.VITE_API_URL + '/items'
+
+export const itemsIndex = () => {
+    return axios.get(BASE_URL)
+
+}
+
+export const itemShow = () => {
+    return axios.get(`${BASE_URL}/${projectId}`)
+}
+
+export const itemCreate = (formData) => {
+    console.log('Posting to:', BASE_URL + '/new')
+    return axios.post(BASE_URL, formData, {
+        headers: {
+            Authorization: `Bearer ${getToken()}`
+        }
+    })
+}
+
+export const itemUpdate = (itemId, formData) => {
+    return axios.put(`${BASE_URL}/${itemId}`, formData, {
+        headers: {
+            Authorization: `Bearer ${getToken()}`
+        }
+    })
+}
+
+export const itemDelete = (itemId) => {
+    return axios.delete(`${BASE_URL}/${itemId}`, {
+        headers: {
+            Authorization: `Bearer ${getToken()}`
+        }
+    })
+}
