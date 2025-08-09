9a28321c5cca94b24624031b24e395f3272b6152
Katie Hill <khill.fr.gr@gmail.com>
2025-08-09 16:21:47 +0100

    Item sections added to home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,17 +1,38 @@
 import './HomePage.css'
 
+import accessoriesImg from '../../assets/accessories-category.jpeg';
+import heroImg from '../../assets/hero-image.jpeg';
+import techImg from '../../assets/tech-category.jpeg';
+import placeholderImg from '../../assets/placeholder.jpeg';
+
 const HomePage = () => {
   return (
     <main>
-    <section class="hero">
+    <section className="hero">
       <h1>Welcome to Re-Lux</h1>
       <p>Discover amazing items and experiences.</p>
     </section>
-    <section class="page-content">
+    <section className="page-content">
+      <div id="categories">
         <h2>Categories</h2>
       <p>Browse the latest items for sale in each category.</p>
+      <button className="page-button">Discover all</button>
+      <div className="image-row">
+    <img src={accessoriesImg} alt="A black luxury watch with a grey background" />
+    <img src={heroImg} alt="Temporary img placeholder" />
+    <img src={techImg} alt="A woman sitting down wearing a virtual reality headset" />
+  </div>
+      </div>
+           <div id="featured-items">
         <h2>Featured</h2>
       <p>Find the best deals and all the latest listings here.</p>
+            <button className="page-button">Shop now</button>
+                  <div className="image-row">
+    <img src={placeholderImg} alt="Text that says Coming soon" />
+    <img src={placeholderImg} alt="Text that says Coming soon" />
+    <img src={placeholderImg} alt="Text that says Coming soon" />
+  </div>
+      </div>
       </section>
     </main>
   )
