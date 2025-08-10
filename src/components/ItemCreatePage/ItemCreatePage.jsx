bc3593e1f6c5493db96a433037cfae90812329ec
Katie Hill <khill.fr.gr@gmail.com>
2025-08-10 14:59:10 +0100

    Item create page styling added

--- src/components/ItemCreatePage/ItemCreatePage.jsx
+++ src/components/ItemCreatePage/ItemCreatePage.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 const ItemCreatePage = () => {
   return (
-    <div>
-      <h1>Sell an item</h1>
-      <p>Create your listing below. Please provide as many details as you can about the product, including its dimensions, condition, origin, and brand.</p>
+    <div className="page-content">
+      <h1>List your item</h1>
+      <p>Please enter your product details, including its dimensions, condition, origin, and brand.</p>
       <section className='form'>
         <ItemCreateForm />
       </section>
-      <Link to="/items">Back to Items</Link>
+      <Link to="/profile">Back to your profile</Link>
     </div>
   )
 }
