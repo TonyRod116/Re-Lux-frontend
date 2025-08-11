cfcff12cedb58a20218c2303043df04ce17e3c2a
Katie Hill <khill.fr.gr@gmail.com>
2025-08-11 12:37:52 +0100

    Cloudinary URL names added & Cloudinary.js added to Services folder for Axios call

---
+++ src/services/cloudinary.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const BASE_URL = import.meta.env.VITE_CLOUDINARY_URL
+
+export const uploadImage = (file) => {
+  return axios.postForm(BASE_URL, {
+    file,
+    upload_preset: import.meta.env.VITE_UPLOAD_PRESET
+  })
+}
+
+// Creates an axios call to your cloud on Cloudinary
