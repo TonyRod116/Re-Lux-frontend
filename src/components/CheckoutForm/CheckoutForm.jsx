b11081817cead8bee4c0765f1dedbfd2d198949d
Katie Hill <khill.fr.gr@gmail.com>
2025-08-12 12:36:55 +0100

    Checkout Page component completed

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -1,6 +1,1 @@
-// import { purchaseIntent } from './services/stripe';
 
-// async function initPayment() {
-//   const clientSecret = await purchaseIntent(5000); // amount in cents
-//   // use clientSecret with Stripe's confirmPayment API
-// }
