498692d12037af6c86212ac575e8f879077e47fb
Katie Hill <khill.fr.gr@gmail.com>
2025-08-12 12:45:15 +0100

    LoadStripe and Publisable key removed from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,6 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import { UserProvider } from './Contexts/UserContext.jsx'
 
-import { CheckoutProvider } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
-
-const stripePromise = loadStripe('pk_test_51RvEnKLg1i0XX0nHB9YGSf3ijavxfPZ5rCxDK4BVRJX0cYwFRz5y1INi4oZbJ1OtTZKB3SZNp73llazJdwpSRJCY00sjvLESC2')
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
