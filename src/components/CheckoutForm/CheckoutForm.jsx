fcc03af435809b48839d853e0eb9c8b482f3b873
Katie Hill <khill.fr.gr@gmail.com>
2025-08-12 15:59:33 +0100

    Checkout form Card Element added

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -2,9 +2,41 @@
 import { useState, useEffect } from 'react'
 import { itemUpdate, itemShow, getItemTypes } from '../../services/items'
 import { useNavigate, useParams } from 'react-router'
+import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
 
 
 const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    // Functions
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+        try {
+            const card = elements.getElement(CardElement);
+            const { error, paymentMethod } = await stripe.createPaymentMethod({
+                type: 'card',
+                card,
+            })
+        }
+
+        catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <CardElement />
+            <button type="submit" disabled={!stripe}>Pay</button>
+        </form>
+    )
+
 }
 
 
