62b3b5dd4711b9b7b8c7184b28611215867a8e3c
Katie Hill <khill.fr.gr@gmail.com>
2025-08-12 11:38:15 +0100

    initPayment function added to CheckoutForm component

---
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -0,0 +1,6 @@
+// import { purchaseIntent } from './services/stripe';
+
+// async function initPayment() {
+//   const clientSecret = await purchaseIntent(5000); // amount in cents
+//   // use clientSecret with Stripe's confirmPayment API
+// }
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,6 @@ import { loadStripe } from '@stripe/stripe-js'
 
 const stripePromise = loadStripe('pk_test_51RvEnKLg1i0XX0nHB9YGSf3ijavxfPZ5rCxDK4BVRJX0cYwFRz5y1INi4oZbJ1OtTZKB3SZNp73llazJdwpSRJCY00sjvLESC2')
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
