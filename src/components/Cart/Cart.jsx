9b3fdd6fac8764f68ec32301c2540ebe5f687ca6
Katie Hill <khill.fr.gr@gmail.com>
2025-08-13 11:18:36 +0100

    Sitewide button styling changed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import './Cart.css'
 
 import { useEffect, useState, useContext } from 'react'
 import { UserContext } from '../../Contexts/UserContext'
+import { Link } from 'react-router-dom'
 
 import { MdModeEdit, MdDelete, MdFavorite } from "react-icons/md"
 
@@ -22,13 +23,15 @@ const Cart = () => {
          <div className="bag-content">
             <h1>Your bag</h1>
               <div className="cart-items">
+                <p>Your bag is empty</p>
+                <Link to="/items" className="page-button">Continue shopping</Link>
  
               </div>
               <div className="order-summary">
                 <h3>Order summary</h3>
                 <p> Total will appear here </p>
                   <div className="button-row">
-                  <button className="checkout-button">Go to checkout</button>
+                  <button className="page-button">Go to checkout</button>
                   </div>
                 </div>
               </div>
