e505c378259db4e4dac854d537686f4c47eb5f56
Katie Hill <khill.fr.gr@gmail.com>
2025-08-14 20:36:34 +0100

    Image added to 404

--- src/components/404NotFound/404NotFound.jsx
+++ src/components/404NotFound/404NotFound.jsx
@@ -1,10 +1,14 @@
 import './404NotFound.css'
+import doodleshoppingbag from '../../assets/doodle-shopping-bag.png';
 
 import { Link } from 'react-router-dom'
 
 const NotFound = () => {
     return (
         <div className="page-content">
+            <div className="notfound-image">
+            <img src={doodleshoppingbag} alt="Empty shopping bag" />
+            </div>
             <h1> Oops, page not found</h1>
             <p>We can't find the page you were looking for.</p>
             <Link to="/items" className="page-button">Continue shopping</Link>
